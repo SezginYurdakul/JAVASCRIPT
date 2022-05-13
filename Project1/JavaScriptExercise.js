5c9664d65151e2b5051a022b82a4cee3cda538c7
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-05-13 11:05:01 +0200

    first 3 exercise is done

--- Project1/JavaScriptExercise.js
+++ Project1/JavaScriptExercise.js
@@ -10,3 +10,12 @@ console.log("Ola mundo");
 console.log("Zdravo svijete");
 
 
+console.log("I'm awesome!");  //console.log('I'm awesome'!; it has a syntax error in it.
+
+
+let numberX;
+console.log("numberX is not initialized, so its result will be error when  I print numberX to console")
+console.log(numberX);
+numberX=25;
+console.log("numberX has value, 25 when  I print numberX to console, i will see 25 on console")
+console.log(numberX);
