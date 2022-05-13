85aa637b17809ff0bd18df166e5a190eef9b7957
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-05-13 11:40:35 +0200

    exercise 45 is done

--- Week1/JavaScriptExercise.js
+++ Week1/JavaScriptExercise.js
@@ -1,3 +1,5 @@
+
+//Excersie 1
 console.log("Hello world!");
 console.log("Halo, dunia!");
 console.log("Ciao, mondo!");
@@ -9,13 +11,32 @@ console.log("Kaixo Mundua");
 console.log("Ola mundo");
 console.log("Zdravo svijete");
 
-
+//Excersie 2
 console.log("I'm awesome!");  //console.log('I'm awesome'!; it has a syntax error in it.
 
-
+//Excersie 3
 let numberX;
 console.log("numberX is not initialized, so its result will be error when  I print numberX to console")
 console.log(numberX);
 numberX=25;
 console.log("numberX has value, 25 when  I print numberX to console, i will see 25 on console")
 console.log(numberX);
+
+//Excersie 4
+let myString="Sezgin Yurdakul";
+console.log("myString value is the string -Sezgin Yurdakul-");
+console.log(myString);
+myString="Salih Yurdakul";
+console.log("myString value is the string -Salih Yurdakul-");
+console.log(myString);
+
+//Excersie 5
+let z=7.25;
+console.log(z);
+let a=7;
+console.log(a);
+
+if(z==a){console.log("z and a variables are equals : ")};
+if(z>a){console.log("z is greater than a")};
+if(z<a){console.log("z is less than a")};
+
