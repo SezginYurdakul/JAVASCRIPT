0c3de87ac664bf4c7280e464fdad417ced637792
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-05-13 10:56:50 +0200

    Java Script exercise file created

---
+++ Project1/JavaScriptExercise.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+console.log("Halo, dunia!");
+console.log("Ciao, mondo!");
+console.log("Hola, mundo!");
+console.log("Merhaba Dunya");
+console.log("Hello Wêreld");
+console.log("Salut Lume");
+console.log("Kaixo Mundua");
+console.log("Ola mundo");
+console.log("Zdravo svijete");
+
+
